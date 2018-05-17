5bc54a4212717783ebb70e0d39525a1e7f178887
Denis Tarkhanov <dtarhanov@mail.ru>
2018-05-17 12:37:42 +0300

    Add Database class

---
+++ lib/Database.php
@@ -0,0 +1,60 @@
+<?php
+
+class Database
+{
+	private static $instance = null;
+	private $connection = null;
+	
+	private function __construct()
+	{
+	}
+	
+	public function getInstance()
+	{
+		if (!self::$instance)
+			self::$instance = new Database();
+		
+		return self::$instance;
+	}
+	
+	public function connection()
+	{
+		return $connection;
+	}
+	
+	public function connect($server, $login, $pass, $dbname)
+	{
+		$this->connection = @mysqli_connect($server, $login, $pass, $dbname);
+		if (!$this->connection)
+		{
+			throw new Exception('error connecting to DB');
+		}
+		
+		mysqli_set_charset($this->connection, 'UTF-8');
+	}
+	
+	public function escapeString($str)
+	{
+		return mysqli_real_escape_string($this->connection, $str);
+	}
+	
+	public function fetch($q)
+	{
+		$r = mysqli_query($this->connection, $q);
+		if (!$r || !mysqli_num_rows($r))
+			return false;
+		
+		return mysqli_fetch_assoc($r);
+	}
+	
+	public function fetchAll($q)
+	{
+		$r = mysqli_query($this->connection, $q);
+		
+		$res = [];
+		
+		// ..
+		
+		return $res;
+	}
+}
--- www/index.php
+++ www/index.php
@@ -3,9 +3,24 @@
 define ('BASE_DIR', __DIR__ . '/../');
 
 require_once BASE_DIR . 'lib/Config.php';
+require_once BASE_DIR . 'lib/Database.php';
 
-Config::load('default.ini');
+try
+{
+	//Config::load('debug.ini');
 
-$config = Config::getInstance();
-
-var_dump($config);
+	$config = Config::getInstance();
+	
+	$db = Database::getInstance();
+	$db->connect($config->get('db.server'), $config->get('db.user'), $config->get('db.pass'), $config->get('db.name'));
+	
+	var_dump($db);
+}
+catch (Exception $ex)
+{
+	// TODO: log error
+	
+	echo 'Sorry, something goes wrong..<br>';
+	echo 'ERROR: ' . $ex->getMessage();
+	die();
+}
